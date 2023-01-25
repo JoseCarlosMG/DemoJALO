735fa27baeb216b386bb0b0835637518cad23e25
José Carlos Méndez Gómez <josecarlosmendezgomez@outlook.com>
2023-01-25 12:53:17 -0600

    Add project files.

---
+++ DemoJALO/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoJALO;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ DemoJALO/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoJALO;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
